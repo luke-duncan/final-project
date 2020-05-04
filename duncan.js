b73dcb71f8f28064cff18724cea51358c1bf2b31
James Luke Duncan <luke@Jamess-MacBook-Pro.local>
2020-05-04 14:28:42 -0400

    updatedscale

--- duncan.js
+++ duncan.js
@@ -5,16 +5,16 @@
 
 var Promise = d3.csv("updatedcscdata.csv");
 
-Promise.then(function(AGE)
-            {console.log('updatedcscdata',AGE);
-             initGraph("#histogram",AGE);
+Promise.then(function(AGES)
+            {console.log('updatedcscdata',AGES);
+             initGraph(AGES,"#histogram");
             },
              
              function(err)
              {console.log('error')}
             )
 
-var initGraph = function(AGE,target)
+var initGraph = function(AGES,target)
 
 {
     var screen = {width:600, height:450};
@@ -27,8 +27,7 @@ var initGraph = function(AGE,target)
         height:screen.height-margins.top-margins.bottom,
     };
     
-   var svg = d3.select("#histogram")
-        .append("svg")
+   var svg = d3.select(target)
         .attr("width", screen.width)
         .attr("height", screen.height);
     
@@ -37,8 +36,15 @@ var initGraph = function(AGE,target)
         .classed("graph",true)
         .attr("transform","translate("+margins.left+","+margins.top+")");
     
-    var xScale = d3.scaleLinear()
-        .domain([0,AGE[0]])
+    var getAGES = function(updatedcscdata)
+    { 
+        var getAge = function(AGE)
+        {return AGE}
+        
+        return AGE.map.getAge}
+    
+    var xScale = d3.scaleBand()
+        .domain([getAGES])
         .range([0,graph.width]);
     
     var yScale = d3.scaleLinear()
@@ -54,8 +60,8 @@ var createAxes = function(screen,margins,graph,target,xScale,yScale)
         var xAxis = d3.axisBottom(xScale);
         var yAxis = d3.axisLeft(yScale);
         
-        var axes = 
-        target.append("g")
+        var axes = d3.select(target)
+        .append("g")
     axes.append("g")
         .attr("transform","translate("+margins.left+","+(margins.top+graph.height)+")")
         .call(xAxis)
@@ -65,6 +71,8 @@ var createAxes = function(screen,margins,graph,target,xScale,yScale)
         
     
     }
+
+//var drawRecs = function()
 
 
 
